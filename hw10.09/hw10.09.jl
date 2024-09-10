8f70067ed4924f9f5700c08bdc7eb3409a303cfb
y0urmercy <lena.sherstuk2006@mail.ru>
2024-09-10 22:17:30 +0300

    Add files via upload

---
+++ hw10.09/hw10.09.jl
@@ -0,0 +1,135 @@
+using HorizonSideRobots
+
+
+function inverse_side(side::HorizonSide)
+    return HorizonSide((Int(side)+2)%4)
+end
+
+
+function move_till_border!(robot::Robot, side::HorizonSide; putmarker::Bool=false)::Integer
+    steps_till_border::Integer = 0
+
+    while (!isborder(robot, side))
+        (putmarker == true) && (putmarker!(robot))
+        HorizonSideRobots.move!(robot, side)
+        steps_till_border += 1
+    end
+
+    (putmarker == true) && (putmarker!(robot))
+
+    return steps_till_border
+end
+
+function move_till_border_chess!(robot::Robot, side::HorizonSide; putmarker::Bool=false)::Integer
+    steps_till_border::Integer = 0
+
+    while (!isborder(robot, side))
+        if (steps_till_border%2==1)
+            (putmarker == true) && (putmarker!(robot))
+        else
+            putmarker == false
+        end
+        HorizonSideRobots.move!(robot, side)
+        steps_till_border += 1
+    end
+
+    (putmarker == true) && (putmarker!(robot))
+
+    return steps_till_border
+end
+
+
+function move_steps!(robot::Robot, side::HorizonSide, steps::Integer; putmarker::Bool=false)::Bool
+    while (steps > 0)
+        if (!isborder(robot, side))
+            (putmarker == true) && (putmarker!(robot))
+            HorizonSideRobots.move!(robot, side)
+            steps -= 1
+        else
+            return false
+        end
+    end
+
+    (putmarker == true) && (putmarker!(robot))
+
+    return true
+end
+
+function move_steps_chess!(robot::Robot, side::HorizonSide, steps::Integer; putmarker::Bool=false)::Bool
+    while (steps > 0)
+        if (!isborder(robot, side))
+            if (steps%2!=1)
+                (putmarker == true) && (putmarker!(robot))
+            else
+                putmarker == false
+            end
+            HorizonSideRobots.move!(robot, side)
+            steps -= 1
+        else
+            return false
+        end
+    end
+
+    (putmarker == true) && (putmarker!(robot))
+
+    return true
+end
+
+function cross!(robot::Robot)
+    for side in [Nord,Ost,West,Sud]
+        steps_till_border::Integer = move_till_border!(robot, side, putmarker=true)
+        move_steps!(robot, inverse_side(side), steps_till_border, putmarker=false)
+    end
+end
+
+function perimetr!(robot::Robot)
+    for side in [Nord,Ost,Sud,West]
+        move_till_border!(robot, side, putmarker=true)
+    end
+end
+
+function perimetr_chess!(robot::Robot)
+    for side in [Nord,Ost,Sud,West]
+        move_till_border_chess!(robot, side, putmarker=true)
+    end
+end
+
+function whole_place!(robot::Robot)
+    side = Ost
+    while !isborder(robot, Nord) || !isborder(robot, Ost) || !isborder(robot, West)
+        while (!isborder(robot, side))
+            putmarker!(robot)
+            HorizonSideRobots.move!(robot, side)
+        end
+        putmarker!(robot)
+        HorizonSideRobots.move!(robot, Nord)
+        side = inverse_side(side)
+    end
+    
+    putmarker!(robot)
+end
+
+function whole_place_chess!(robot::Robot)
+    side = Ost
+    steps = 0
+    while !isborder(robot, Nord) || !isborder(robot, Ost) || !isborder(robot, West)
+        while (!isborder(robot, side))
+            if (steps%2==0)
+                putmarker!(robot)
+            end
+            HorizonSideRobots.move!(robot, side)
+            steps +=1
+        end
+        if (steps%2==0)
+            putmarker!(robot)
+        end
+        steps +=1
+        HorizonSideRobots.move!(robot, Nord)
+        side = inverse_side(side)
+    end
+    
+    putmarker!(robot)
+end
+
+r = Robot(;animate=true)
+whole_place_chess!(r)
